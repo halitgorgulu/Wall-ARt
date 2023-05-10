344e2d18449c7fc38047721b145f37c892629a82
halitgorgulu <halitgorgulu01@gmail.com>
2023-05-10 14:38:03 +0300

    some improvements

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,7 +11,7 @@ function Header() {
                 <div className="col-md-3 col-sm-3">
                   <div className="site-title">
                     <a href="/#home" style={{ textDecoration: "none" }}>
-                      <h3>Wall-ARt</h3>
+                      <img src="/assets/img/logo_new.png" width="auto" height="75"></img>
                     </a>
                   </div>
                 </div>
